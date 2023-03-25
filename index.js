2c538251b4c3779755f44a654b7a3aa22a25aba0
Benah0987 <88454033+Benah0987@users.noreply.github.com>
2023-03-25 23:24:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let left = parseInt(dodger.style.left);
+  if (left > 0) {
+    dodger.style.left = `{left - 1}"px"`;
+  }
+}
+
+function moveDodgerRight() {
+  let left = parseInt(dodger.style.left);
+  let right = left + 40;
+  
+  if (right < ) {
+    dodger.style.left = `{left + 1}"px"`;
+  }
+}
+//a function to prevent the dodger from escaping
+function moveDodger(direction) {
+  switch(direction) {
+    case "left":
+      moveDodgerLeft();
+      break;
+    case "right":
+      moveDodgerRight();
+      break;
+    // Add additional cases for other directions
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key.startsWith("Arrow")) {
+    moveDodger(e.key.substring(5).toLowerCase());
+  }
+});
